9ae2eed70ebe49e6ee13250517312e677518936d
Bruce Daza <ferbruce08@gmail.com>
2025-06-27 15:40:00 -0500

    GenericList

---
+++ Compuserver/Compuserver.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,18 @@
+using Compuserver.Shared.Entites;
+using Fantasy.Frontend.Repositories;
+using Microsoft.AspNetCore.Components;
+
+namespace Compuserver.Frontend.Pages.Countries;
+
+public partial class CountriesIndex
+{
+    [Inject] private IRepository Repository { get; set; } = null!;
+
+    private List<Country>? Countries { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var responseHppt = await Repository.GetAsync<List<Country>>("api/Countries");
+        Countries = responseHppt.Response!;
+    }
+}
---
+++ Compuserver/Compuserver.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Compuserver.Frontend.Shared;
+
+public partial class GenericList<TItem>
+{
+    [Parameter] public RenderFragment? Loading { get; set; }
+    [Parameter] public RenderFragment? NoRecords { get; set; }
+    [EditorRequired, Parameter] public RenderFragment Body { get; set; } = null!;
+    [EditorRequired, Parameter] public List<TItem> MyList { get; set; } = null!;
+}
